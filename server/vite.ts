a0ac1fdadefc3d23ca598225ac26f70a0a29510e
LethalAssassin26 <caidenbroadfield5@gmail.com>
2026-02-10 23:07:50 +0000

    .

--- server/vite.ts
+++ server/vite.ts
@@ -79,7 +79,8 @@ export function serveStatic(app: Express) {
   app.use(express.static(distPath));
 
   // fall through to index.html if the file doesn't exist
-  app.use("*", (_req, res) => {
-    res.sendFile(path.resolve(distPath, "index.html"));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "public", "index.html"));
   });
+
 }
